3e2debb957168b4ae4ec28a0dd71c4abc7ee4cd8
Michel <leyou.m@gmail.com>
2013-07-04 00:21:17 +0200

    update

---
+++ coxis/composer/AppInstaller.php
@@ -0,0 +1,24 @@
+<?php
+namespace Coxis\Composer;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class AppInstaller extends LibraryInstaller
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        return 'app/'.basename(substr($package->getPrettyName(), 0, 23));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        return $packageType === 'coxis-app';
+    }
+}
